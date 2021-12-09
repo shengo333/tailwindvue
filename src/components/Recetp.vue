4481c672ee52e03e29bf429fe990e3aa4833b987
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-09 15:47:35 +0400

    v-for cards created

--- src/components/Recetp.vue
+++ src/components/Recetp.vue
@@ -15,52 +15,10 @@
                 <h4 class="font-bold mt-12 pb-2 border-b border-gray-200"> Latest Recipes</h4>
                 <div class="mt-8 grid lg:grid-cols-3 gap-10"> 
                     <!-- cards go here-->
-                    <RecepCard />
-                    <div class="card hover:shadow-lg">
-                        <img src="../assets/stew.jpg" alt="stew" class="w-full h-32 sm:h-48 object-cover">
-                        <div class="m-4">
-                        <router-link to="/Stew">
-                                <span class="font-bold"> 5 Bean Chilli Stew</span>
-                                <span class="block text-gray-500 text-sm">Recipe from Mario</span>
-                            </router-link>
-                        </div>
-                        <div class="badge">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                              </svg>
-                            <span>25 Mins</span>
-                        </div>
-                    </div>
-                    <div class="card hover:shadow-lg">
-                        <img src="../assets/noodles.jpg" alt="noodles" class="w-full h-32 sm:h-48 object-cover">
-                        <div class="m-4">
-                            <router-link to="/noodles">
-                                <span class="font-bold"> Veg Noodles</span>
-                                <span class="block text-gray-500 text-sm">Recipe from Mario</span>
-                            </router-link>
-                        </div>
-                        <div class="badge">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                              </svg>
-                            <span>25 Mins</span>
-                        </div>
-                    </div>
-                    <div class="card hover:shadow-lg">
-                        <img src="../assets/curry.jpg" alt="curry" class="w-full h-32 sm:h-48 object-cover">
-                        <div class="m-4">
-                            <router-link to="/curry">
-                                <span class="font-bold"> Tofu Curry</span>
-                                <span class="block text-gray-500 text-sm">Recipe from Mario</span>
-                            </router-link>
-                        </div>
-                        <div class="badge">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                              </svg>
-                            <span>25 Mins</span>
-                        </div>
-                    </div>
+                    <RecepCard v-for="recipe in recipes" :key="recipe.title"/>
+
+
+
                 </div>
                 <h4 class="font-bold mt-12 pb-2 border-b border-gray-200 font">Most populars</h4>
                 <div class="mt-8">
