40391da87cb317610b9a19b0a1721df4043b5084
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-14 15:07:20 +0400

    some more content

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,12 +17,16 @@
     
     <div class="clearfix">
       <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
+      <button type="submit" class="signupbtn">Log In</button>
     </div>
   </div>
 </form>
 
 </template>
+
+
+
+
 
 <style scoped>
 body {
